524db7fbd1109bfdf851e0571e6e4271160a46cc
Dino Omanovic <angor.mail@googlemail.com>
2017-11-25 13:07:45 +0100

    fixed websocket handling

--- broadcast/broadcast.go
+++ broadcast/broadcast.go
@@ -1,47 +1,47 @@
 package broadcast
 
-import (
-	"github.com/gorilla/websocket"
-	"github.com/pkg/errors"
-	"sync"
-)
+// import (
+// 	"github.com/gorilla/websocket"
+// 	"github.com/pkg/errors"
+// 	"sync"
+// )
 
-//TODO: Add graceful Close method
-type BroadCaster interface {
-	Add(conn *websocket.Conn) 
-	Remove(conn *websocket.Conn)
-	Write(msg []byte) (n int, err error)
-}
+// //TODO: Add graceful Close method
+// type BroadCaster interface {
+// 	Add(conn *websocket.Conn) 
+// 	Remove(conn *websocket.Conn)
+// 	Write(msg []byte) (n int, err error)
+// }
 
-type broadCaster struct {
-	connections map[*websocket.Conn]struct{}
-	sync.RWMutex
-}
+// type broadCaster struct {
+// 	clients map[*Client]struct{}
+// 	sync.RWMutex
+// }
 
-func (bc *broadCaster) Add(conn *websocket.Conn) {
-	bc.Lock()
-	defer bc.Unlock()
-	bc.connections[conn] = struct{}{}
-}
+// func (bc *broadCaster) Add(conn *websocket.Conn) {
+// 	bc.Lock()
+// 	defer bc.Unlock()
+// 	bc.connections[conn] = struct{}{}
+// }
 
-func NewBroadCaster() *broadCaster{
-	return &broadCaster{connections: make(map[*websocket.Conn]struct{})}
-}
+// func NewBroadCaster() *broadCaster{
+// 	return &broadCaster{clients: make(map[*Client]struct{})}
+// }
 
-func (bc *broadCaster) Remove(conn *websocket.Conn) {
-	bc.Lock()
-	defer bc.Unlock()
-	delete(bc.connections, conn)
-}
+// func (bc *broadCaster) Remove(conn *websocket.Conn) {
+// 	bc.Lock()
+// 	defer bc.Unlock()
+// 	delete(bc.connections, conn)
+// }
 
-// Write accepts serialized model.Message objects
-func (bc *broadCaster) Write(msg []byte) (n int, err error) {
-	bc.RLock()
-	defer bc.RUnlock()
-	for conn := range bc.connections {
-		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
-			return 0, errors.Wrap(err, "could not write message to broadcaster")
-		}
-	}
-	return len(msg), nil
-}
+// // Write accepts serialized model.Message objects
+// func (bc *broadCaster) Write(msg []byte) (n int, err error) {
+// 	bc.RLock()
+// 	defer bc.RUnlock()
+// 	for conn := range bc.connections {
+// 		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+// 			return 0, errors.Wrap(err, "could not write message to broadcaster")
+// 		}
+// 	}
+// 	return len(msg), nil
+// }
--- main.go
+++ main.go
@@ -2,59 +2,19 @@ package main
 
 import (
 	"net/http"
-	"github.com/gorilla/websocket"
 	"github.com/domano/go-react-chat/broadcast"	
-	"github.com/domano/go-react-chat/model"	
-	"encoding/json"
 	log "github.com/sirupsen/logrus"
 )
 
-var messages chan string = make(chan string)
- 
-var bc  = broadcast.NewBroadCaster()
-
-var upgrader = websocket.Upgrader{} // use default options
-
 var logger = log.StandardLogger()
 
-func ChatServer(rw http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(rw, r, nil)
-	if err != nil {
-		logger.WithError(err).Error("Failed upgrade")
-		return
-	}
-	defer conn.Close()
-	bc.Add(conn)
-	defer bc.Remove(conn)
-	encoder := json.NewEncoder(bc)
-	for {
-		_, reader, err := conn.NextReader()
-		if err != nil {
-			logger.WithError(err).Error("Failed reading")
-			return
-		}
-
-		msg := &model.Message{}
-		if err := json.NewDecoder(reader).Decode(msg); err != nil {
-			logger.WithError(err).Error("Failed decoding")
-			return
-		}
-		
-		logger.WithField("message", msg).Info("Sending msg")
-		// TODO: Do msg validation here
-			if err := encoder.Encode(msg); err != nil {
-				logger.WithError(err).Error("Failed encoding")
-				return
-			}
-	}
-}
-
 func main() {
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
 
-	http.HandleFunc("/chat", ChatServer)
+	hub := broadcast.NewHub()
+	go hub.Run()
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		broadcast.ServeWs(hub, w, r)
+	})
 	println("Run server...")
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
